6eec895a92652c35830efad7f7e2ed1a33df2a1b
Mark Wnuk <marwnuk@yahoo.com>
2017-07-16 21:26:43 -0400

    Updated

--- TimeSeries/TimeSeries.py
+++
@@ -1,8 +0,0 @@
-#https://www.analyticsvidhya.com/blog/2016/02/time-series-forecasting-codes-python/
-import pandas as pd
-import numpy as np
-import matplotlib.pylab as plt
-#%matplotlib inline
-from matplotlib.pylab import rcParams
-rcParams['figure.figsize'] = 15, 6
-
